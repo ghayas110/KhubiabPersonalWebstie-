d8b658686f81e63bd04c6ded9d9bfe0f2d7170c5
Ghayas Ali <55618250+ghayas110@users.noreply.github.com>
2024-08-28 15:57:34 +0500

    about kubaib

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,9 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
 <div>
-  <h1 className="text-3xl font-bold underline">Kubabibb</h1>
+  <h1 className="text-3xl font-bold underline">Kubabibb Mushtaq</h1>
+  <p>I am a Web Developer </p>
+  <p>I love to code</p>
 </div>
   );
 }
